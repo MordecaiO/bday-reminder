526fbdbd92ee472e7a29a236f7f752eac2b39de6
mordecaioladimeji <cai.oladimeji@gmail.com>
2022-11-16 15:12:13 +0000

    added basic structure of app : List component, container and clear all button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import List from "./List";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="list-container">
+        <h1>0 birthdays today</h1>
+        <List></List>
+        <button>Clear All</button>
+      </section>
+    </main>
   );
 }
 
