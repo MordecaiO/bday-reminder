b794c94c1620021b9eb721e5a251a85aef67facc
mordecaioladimeji <cai.oladimeji@gmail.com>
2022-11-16 15:05:30 +0000

    added content to the objects in data.js file

--- src/data.js
+++ src/data.js
@@ -1,32 +1,23 @@
-[
-    {
-        id: 1,
-        name: ,
-        age: ,
-        image: ,
-    },
-    {
-        id: 1,
-        name: ,
-        age: ,
-        image: ,
-    },
-    {
-        id: 1,
-        name: ,
-        age: ,
-        image: ,
-    },
-    {
-        id: 1,
-        name: ,
-        age: ,
-        image: ,
-    },
-    {
-        id: 1,
-        name: ,
-        age: ,
-        image: ,
-    },
-]
+export const data = [
+  {
+    id: 1,
+    name: "Larry",
+    age: 65,
+    image:
+      "http://res.cloudinary.com/ybmedia/image/upload/c_crop,h_1123,w_2000,x_0,y_329/c_fill,f_auto,h_900,q_auto,w_1600/v1/m/b/9/b9ee9ffab2e220c756427b601a4690c5d294c963/larry-bird-career-retrospective.jpg",
+  },
+  {
+    id: 2,
+    name: "Stephen",
+    age: 31,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/a/aa/TechCrunch_Disrupt_2019_%2848834853256%29_%281%29.jpg,",
+  },
+  {
+    id: 3,
+    name: "Magic",
+    age: 63,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Magic_Johnson_at_SXSW_2022_%2851958828669%29_%28cropped%29.jpg/1200px-Magic_Johnson_at_SXSW_2022_%2851958828669%29_%28cropped%29.jpg",
+  },
+];
